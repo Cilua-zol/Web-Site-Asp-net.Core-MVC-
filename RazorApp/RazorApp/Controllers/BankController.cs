18fb30513610a1e7b1a436eac9732a1a6d398c32
Caska <temautch@yandex.ru>
2022-04-17 22:20:24 +0300

    1

--- RazorApp/RazorApp/Controllers/BankController.cs
+++ RazorApp/RazorApp/Controllers/BankController.cs
@@ -1,10 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RazorApp.Methods;
+using RazorApp.Models;
 
 namespace RazorApp.Controllers
 {
 
     public class BankController : Controller
     {
+        private readonly IWalletMethods _wallet;
+        private readonly ITestMethods _test;
+        public BankController(ITestMethods test, IWalletMethods wallet)
+        {
+            _test = test;
+            _wallet = wallet;
+            
+            
+        }
+        [HttpGet]
+        public IActionResult CreateWallet()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateWallet(string cardNumber, string password, string phoneNumber)
+        {
+            Wallet wallet = await _wallet.CreateWallet(cardNumber, password, phoneNumber);
+            return RedirectToAction("WalletResult", "Bank", wallet);
+        }
+        [HttpGet]
+        public IActionResult WalletResult(Wallet wallet)
+        {
+            return View(wallet);
+        }
         
     }
 }
--- RazorApp/RazorApp/Controllers/HomeController.cs
+++ RazorApp/RazorApp/Controllers/HomeController.cs
@@ -27,25 +27,7 @@ public class HomeController : Controller
     {
         return View();
     }
-
     
-    [HttpGet]
-    public IActionResult Test()
-    {
-        return View();
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> Test(string cardNumber, string password, string phoneNumber)
-    {
-        Wallet wallet = await _wallet.CreateWallet(cardNumber, password, phoneNumber);
-        return RedirectToAction("TestResult", "Home", wallet);
-    }
-    [HttpGet]
-    public IActionResult TestResult(Wallet wallet)
-    {
-        return View(wallet);
-    }
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
